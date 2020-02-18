0d6e413ec9e2becf2f4846086157a6c0b0ca3c4d
Angel <you@example.com>
2020-02-18 17:35:31 -0500

    first commit

--- Principal.java
+++ Principal.java
@@ -1,23 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
 import data.Contacto;
-
 /**
  * Principal
  */
 public class Principal {
-
     public static void main(String[] args) {
-        System.out.println("Hola Java");
+        List<Contacto> listaContactos= new ArrayList<Contacto>();
+
         Contacto c = new Contacto();
         c.setId(1);
         c.setNombre("Juan Perez");
         c.setCelular("99999");
-        System.out.println(c.getId()+" "+c.getNombre());
+        listaContactos.add(c);
 
         Contacto c2 = new Contacto();
         c2.setId(2);
         c2.setNombre("Maria");
         c2.setCelular("88888");
-        System.out.println(c2.getId()+" "+c2.getNombre());
+        listaContactos.add(c2);
 
+        for(Contacto d:listaContactos){
+            System.out.println(d.getId() + "\t"+d.getNombre()); 
+        }
     }
 }
