17b8430989e819952caced7f1099420753e1a461
Angel <you@example.com>
2020-02-19 14:59:57 -0500

    ContactoData

--- Principal.java
+++ Principal.java
@@ -1,27 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
 import data.Contacto;
+import data.ContactoData;
 /**
  * Principal
  */
 public class Principal {
     public static void main(String[] args) {
-        List<Contacto> listaContactos= new ArrayList<Contacto>();
-
+        ContactoData contactoData= new ContactoData();
+        
         Contacto c = new Contacto();
         c.setId(1);
         c.setNombre("Juan Perez");
         c.setCelular("99999");
-        listaContactos.add(c);
+        contactoData.create(c);
 
         Contacto c2 = new Contacto();
         c2.setId(2);
         c2.setNombre("Maria");
         c2.setCelular("88888");
-        listaContactos.add(c2);
+        contactoData.create(c2);
 
-        for(Contacto d:listaContactos){
-            System.out.println(d.getId() + "\t"+d.getNombre()); 
-        }
+        contactoData.list();
+        //for(Contacto d:contactoData.list() ){
+        //    System.out.println(d.getId() + "\t"+d.getNombre()); 
+        //}
+        
     }
 }
--- data/ContactoData.java
+++ data/ContactoData.java
@@ -1,15 +1,23 @@
 package data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * ContactoData DAO está el CRUD
  */
 public class ContactoData {
+    List<Contacto> listaContactos= new ArrayList<Contacto>();
 
     public void list() {
-        System.out.println("list");
+        System.out.println("Lista de contactos");
+        for(Contacto d:listaContactos ){
+            System.out.println(d.getId() + "\t"+d.getNombre()); 
+        }
     }
-    public void create() {
-        System.out.println("create");
+    public void create(Contacto d) {
+        listaContactos.add(d);
+        System.out.println(d.getNombre()+" is created!");
     }
     
 
