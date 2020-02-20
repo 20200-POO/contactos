eca4e1fddb8a151970ca807571f1ea7b5171acc1
Angel <you@example.com>
2020-02-20 15:26:46 -0500

    Menu item action

--- Principal2.java
+++ Principal2.java
@@ -1,5 +1,7 @@
 import java.awt.*;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Principal2 {
     public static void main(String args[]) {
@@ -17,6 +19,19 @@ public class Principal2 {
 
         JMenuItem m11 = new JMenuItem("Contactos");
         JMenuItem m12 = new JMenuItem("Salir");
+
+        m11.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.out.println("Ir a contactos");;
+          }
+        });
+
+        m12.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+                System.exit(0);
+          }
+        });
+
         m1.add(m11);
         m1.add(m12);
 
@@ -25,7 +40,7 @@ public class Principal2 {
         JLabel label = new JLabel("2020 POO");
         panel.add(label);
 
-        
+
         // Agregar componentes al marco.
         frame.add(BorderLayout.NORTH, mb);
         frame.add(BorderLayout.SOUTH, panel);
