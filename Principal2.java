9491123fbd3462bb82b29760e2e6d961b772bd9d
Angel <you@example.com>
2020-03-03 16:14:16 -0500

    menu ok

--- Principal2.java
+++ Principal2.java
@@ -1,11 +1,46 @@
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
+import data.Contacto;
+import data.ContactoData;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 public class Principal2 extends JFrame {
-    public Principal2(){
-        //JFrame frame = new JFrame("Chat Frame");
+
+    public Principal2() {
+        /////
+        ContactoData contactoData = new ContactoData();
+        Contacto c = new Contacto();
+        c.setId(1);
+        c.setNombre("Juan Perez");
+        c.setCelular("99999");
+        contactoData.create(c);
+
+        Contacto c2 = new Contacto();
+        c2.setId(2);
+        c2.setNombre("Maria");
+        c2.setCelular("88888");
+        contactoData.create(c2);
+        ///
+        List<Contacto> miLista = contactoData.list();
+
+        String[][] matrizInfo = new String[miLista.size()][2];
+
+        for (int i = 0; i < miLista.size(); i++) {
+            matrizInfo[i][0] = miLista.get(i).getId() + "";
+            matrizInfo[i][1] = miLista.get(i).getNombre() + "";
+        }
+
+        String[] columnNames = { "Id", "Nombre" };
+        JTable jTableContactos = new JTable(matrizInfo, columnNames);
+        JScrollPane spContactos = new JScrollPane(jTableContactos);
+        /////
+
+        // JFrame frame = new JFrame("Chat Frame");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(400, 400);
 
@@ -20,14 +55,15 @@ public class Principal2 extends JFrame {
         JMenuItem m12 = new JMenuItem("Productos");
         JMenuItem m19 = new JMenuItem("Salir");
 
-        JPanel panelContacto = new JPanel(); 
+        JPanel panelContacto = new JPanel();
         panelContacto.setLayout(new BoxLayout(panelContacto, BoxLayout.Y_AXIS));
         JLabel lblNombre = new JLabel("Ingrese Contacto:");
         JTextField txtNombre = new JTextField();
         panelContacto.add(lblNombre);
         panelContacto.add(txtNombre);
+        panelContacto.add(spContactos);
 
-        JPanel panelProducto = new JPanel(); 
+        JPanel panelProducto = new JPanel();
         panelProducto.setLayout(new BoxLayout(panelProducto, BoxLayout.Y_AXIS));
         JLabel lblPNombre = new JLabel("Ingrese Producto:");
         panelProducto.add(lblPNombre);
@@ -36,20 +72,19 @@ public class Principal2 extends JFrame {
             public void actionPerformed(ActionEvent event) {
                 System.out.println("Ir a contactos");
                 JOptionPane.showMessageDialog(null, panelContacto, "Contactos", JOptionPane.PLAIN_MESSAGE);
-          }
+            }
         });
         m12.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.out.println("Ir a productos");
                 JOptionPane.showMessageDialog(null, panelProducto, "Productos", JOptionPane.PLAIN_MESSAGE);
-          }
+            }
         });
-
 
         m19.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.exit(0);
-          }
+            }
         });
 
         m1.add(m11);
@@ -57,7 +92,7 @@ public class Principal2 extends JFrame {
         m1.add(m19);
 
         // Creando el panel en la parte inferior y agregando componentes
-        JPanel panel = new JPanel(); 
+        JPanel panel = new JPanel();
         JLabel label = new JLabel("2020 POO");
         panel.add(label);
 
@@ -66,11 +101,12 @@ public class Principal2 extends JFrame {
         add(BorderLayout.SOUTH, panel);
 
     }
+
     public static void main(String args[]) {
         // Creando el Marco
         Principal2 ex = new Principal2();
         ex.setVisible(true);
-        
+
     }
-    
+
 }
