b6154fe2b0a76eefcdd8fe6c50c23db6c9dcada7
Angel <you@example.com>
2020-03-04 15:05:52 -0500

    estandarizado

--- Principal2.java
+++ Principal2.java
@@ -9,13 +9,18 @@ import java.awt.event.ActionListener;
 import java.util.List;
 
 public class Principal2 extends JFrame {
-    int id = 0;
+    /**
+     * Descripción de la clase
+     */
+    private static final long serialVersionUID = 1L;
+    // CRUD Contacto
+    int contactoId = 0;
     ContactoData contactoData = new ContactoData();
-    List<Contacto> miLista;
-    String[][] matrizInfo = new String[0][2];;
-    String[] columnNames = { "Id", "Nombre" };
-    JTable jTableContactos ;
-    JScrollPane spContactos;
+    String[][] contactoMatriz = new String[0][2];;
+    String[] contactoColumns = { "Id", "Nombre" };
+    JTable contactoTable = new JTable(contactoMatriz, contactoColumns);
+    JScrollPane contactoSP = new JScrollPane();
+    // CRUD Contacto End
 
     public Principal2() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -26,97 +31,83 @@ public class Principal2 extends JFrame {
         JMenu m2 = new JMenu("AYUDA");
         mb.add(m1);
         mb.add(m2);
-
         JMenuItem m11 = new JMenuItem("Contactos");
         JMenuItem m12 = new JMenuItem("Productos");
         JMenuItem m19 = new JMenuItem("Salir");
 
-
-
-
-        JPanel panelContacto = new JPanel();
-        panelContacto.setLayout(new BoxLayout(panelContacto, BoxLayout.Y_AXIS));
-        JLabel lblNombre = new JLabel("Ingrese Contacto:");
-        JTextField txtNombre = new JTextField();
-        JButton b = new JButton("Add");
-        b.addActionListener(new ActionListener() {
+        // CRUD Contacto
+        JPanel contactoPanel = new JPanel();
+        contactoPanel.setLayout(new BoxLayout(contactoPanel, BoxLayout.Y_AXIS));
+        JLabel contactoLblNombre = new JLabel("Ingrese Contacto:");
+        JTextField contactoTxtNombre = new JTextField();
+        JButton contactoBtnAdd = new JButton("Add");
+        contactoBtnAdd.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                // txtNombre.setText("Welcome to Javatpoint.");
-                System.out.println("txtNombre=" + txtNombre.getText());
-                id++;
+                contactoId++;
                 Contacto d = new Contacto();
-                d.setId(id);
-                d.setNombre(txtNombre.getText());
+                d.setId(contactoId);
+                d.setNombre(contactoTxtNombre.getText());
                 contactoData.create(d);
 
-                miLista = contactoData.list();
-                System.out.println(miLista);
-                matrizInfo = new String[miLista.size()][2];
+                List<Contacto> miLista = contactoData.list();
+                contactoMatriz = new String[miLista.size()][2];
                 for (int i = 0; i < miLista.size(); i++) {
-                    matrizInfo[i][0] = miLista.get(i).getId() + "";
-                    matrizInfo[i][1] = miLista.get(i).getNombre() + "";
+                    contactoMatriz[i][0] = miLista.get(i).getId() + "";
+                    contactoMatriz[i][1] = miLista.get(i).getNombre() + "";
                 }
-                jTableContactos = new JTable(matrizInfo, columnNames);
-                spContactos.setViewportView(jTableContactos);
+                contactoTable = new JTable(contactoMatriz, contactoColumns);// f5 table
+                contactoSP.setViewportView(contactoTable);// f5 table
 
             }
         });
-        jTableContactos = new JTable(matrizInfo, columnNames);
-        spContactos = new JScrollPane();
-        spContactos.setViewportView(jTableContactos);
 
-        panelContacto.add(lblNombre);
-        panelContacto.add(txtNombre);
-        panelContacto.add(b);
-        panelContacto.add(spContactos);
+        contactoSP.setViewportView(contactoTable);
+        contactoPanel.add(contactoLblNombre);
+        contactoPanel.add(contactoTxtNombre);
+        contactoPanel.add(contactoBtnAdd);
+        contactoPanel.add(contactoSP);
+        // CRUD Contacto End
 
+        // CRUD Producto
+        JPanel productoPanel = new JPanel();
+        productoPanel.setLayout(new BoxLayout(productoPanel, BoxLayout.Y_AXIS));
+        JLabel productoLblNombre = new JLabel("Ingrese Producto:");
 
+        productoPanel.add(productoLblNombre);
+        // CRUD Producto End
 
-
-
-
-
-        JPanel panelProducto = new JPanel();
-        panelProducto.setLayout(new BoxLayout(panelProducto, BoxLayout.Y_AXIS));
-        JLabel lblPNombre = new JLabel("Ingrese Producto:");
-
-
-        panelProducto.add(lblPNombre);
-
-
-
-        ///actions
+        // Actions del JFrame
         m11.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.out.println("Ir a contactos");
-                JOptionPane.showMessageDialog(null, panelContacto, "Contactos", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, contactoPanel, "Contactos", JOptionPane.PLAIN_MESSAGE);
             }
         });
         m12.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.out.println("Ir a productos");
-                JOptionPane.showMessageDialog(null, panelProducto, "Productos", JOptionPane.PLAIN_MESSAGE);
+                JOptionPane.showMessageDialog(null, productoPanel, "Productos", JOptionPane.PLAIN_MESSAGE);
             }
         });
-
         m19.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent event) {
                 System.exit(0);
             }
         });
+        // Actions del JFrame End
 
         m1.add(m11);
         m1.add(m12);
         m1.add(m19);
 
         // Creando el panel en la parte inferior y agregando componentes
-        JPanel panel = new JPanel();
-        JLabel label = new JLabel("2020 POO");
-        panel.add(label);
+        JPanel footPanel = new JPanel();
+        JLabel footLblCopy = new JLabel("c(2020) POO");
+        footPanel.add(footLblCopy);
 
         // Agregar componentes al marco.
         add(BorderLayout.NORTH, mb);
-        add(BorderLayout.SOUTH, panel);
+        add(BorderLayout.SOUTH, footPanel);
 
     }
 
